31967323b20b2ff914accdd44a65a898b9bea2e8
Amore <me.thefarkhodov@gmail.com>
2023-09-18 12:35:40 +0500

    Update token_manager.py

--- shizu/bot/token_manager.py
+++ shizu/bot/token_manager.py
@@ -10,53 +10,60 @@ from .types import Item
 
 FIND = True
 
+
 class TokenManager(Item):
     """Менеджер токенов"""
-    
-    async def _find_bot(self) -> Union[str, None]:
-        """Найти бота"""
+
+    async def _find_bot(self) -> Union[Tuple[str, str], None]:
+        """Find the bot"""
         async with fsm.Conversation(self._app, "@BotFather", True) as conv:
-            logging.info("Проверка наличия бота...")
-            
+            logging.info("Checking for the presence of a bot...")
+
             try:
                 await conv.ask("/cancel")
             except errors.UserIsBlocked:
                 await self._app.unblock_user("@BotFather")
-    
+
             await conv.get_response()
-            await conv.ask('/mybots')
+            await conv.ask("/mybots")
             r = await conv.get_response()
-            
+
             if not r.reply_markup:
-                return False
-            
+                return None  # Bot not found
+
             data = r.reply_markup.inline_keyboard
             buttons_text = [button.text for row in data for button in row]
-            
+
             if not buttons_text:
-                logger.error("The bot will not find, an attempt to create a new bot...")
-                return False
-            
-            buttons = [index for index, item in enumerate(buttons_text) if "shizu" in item.lower()]
-            logger.success("Найден бот %s", buttons_text)
+                logger.error("The bot was not found, attempting to create a new bot...")
+                return None  # Bot not found
+
+            buttons = [
+                index
+                for index, item in enumerate(buttons_text)
+                if "shizu" in item.lower()
+            ]
+            logger.success("Found bot: %s", buttons_text[buttons[0]])
             resp = await conv.get_response()
             await resp.click(buttons[0])
-            
+
             h2 = await conv.get_another_same()
             await h2.click(0)
-            
+
             response = await conv.get_response()
-            token_match = re.search(r'(\d+:[A-Za-z0-9_-]+)', response.text)
-            username_match = re.search(r'@(\w+)', response.text)
-            
-            return token_match[1], username_match[1]
-    
-                
+            token_match = re.search(r"(\d+:[A-Za-z0-9_-]+)", response.text)
+            username_match = re.search(r"@(\w+)", response.text)
+
+            if token_match and username_match:
+                return token_match[1], username_match[1]
+            else:
+                return None  # Bot not found
+
     async def _create_bot(self) -> Union[str, None]:
-        """Create and configure a bot"""         
+        """Create and configure a bot"""
         async with fsm.Conversation(self._app, "@BotFather", True) as conv:
             logging.info("The process of creating a new bot has begun...")
-            
+
             try:
                 await conv.ask("/cancel")
             except errors.UserIsBlocked:
@@ -67,18 +74,22 @@ class TokenManager(Item):
             response = await conv.get_response()
 
             error_phrases = ["That I cannot do.", "Sorry"]
-            
+
             if any(phrase in response.text for phrase in error_phrases):
-                logging.error("An error occurred when creating the bot. @BotFather's response:")
+                logging.error(
+                    "An error occurred when creating the bot. @BotFather's response:"
+                )
                 logging.error(response.text)
 
-                if 'too many attempts' in response.text:
+                if "too many attempts" in response.text:
                     seconds = response.text.split()[-2]
-                    logger.error(f'Please repeat in {seconds} seconds')
+                    logger.error(f"Please repeat in {seconds} seconds")
 
             bot_username = f"shizu_{utils.random_id(6)}_bot"
 
-            await conv.ask(f"🐙 Shizu UserBot of {utils.get_display_name(self._all_modules.me)[:45]}")
+            await conv.ask(
+                f"🐙 Shizu UserBot of {utils.get_display_name(self._all_modules.me)[:45]}"
+            )
             await conv.get_response()
 
             await conv.ask(bot_username)
@@ -87,7 +98,9 @@ class TokenManager(Item):
 
             search = re.search(r"(?<=<code>)(.*?)(?=</code>)", response.text.html)
             if not search:
-                logging.error("An error occurred when creating the bot. @BotFather's response:")
+                logging.error(
+                    "An error occurred when creating the bot. @BotFather's response:"
+                )
                 return logging.error(response.text)
 
             token = search[0]
@@ -114,5 +127,3 @@ class TokenManager(Item):
             logger.success(f"Bot successfully created @{bot_username}")
 
             return token, bot_username
-        
-    
